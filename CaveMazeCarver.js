5c4e480947c905ea04a2c0b6cab94d1494400232
tibiovesea <tibiovesea@gmail.com>
2016-08-09 20:18:01 -0400

    -cave carver now has connectin sbetween caves
    -adds seeded random number generator

--- CaveMazeCarver.js
+++ CaveMazeCarver.js
@@ -10,6 +10,7 @@ function CaveMazeCarver(mz) {
 
     this.currentRegion = 0;
     this.regions = [];
+    this.connectionEnds = [];
 
     this.sparseness = 0.50;
     this.generations = 3;
@@ -36,6 +37,119 @@ CaveMazeCarver.prototype.generate = function() {
 
     this.makeCaverns();
     this.identifyRegions();
+    this.findClosestCellsBetweenRegions();
+    this.linkRegions();
+};
+CaveMazeCarver.prototype.findClosestCellsBetweenRegions = function() {
+    for (var r0 of this.regions) {
+        for (var r1 of this.regions) {
+            if (r0.id === r1.id) {
+                continue;
+            }
+
+            var md = -1;
+            var c0, c1;
+            for (var e0 of r0.regionEdges) {
+                for (var e1 of r1.regionEdges) {
+                    var cd = (e1.x - e0.x) * (e1.x - e0.x) + (e1.y - e0.y) * (e1.y - e0.y);
+                    if (md === -1 || cd <= md) {
+                        md = cd;
+                        c0 = e0;
+                        c1 = e1;
+                    }
+                }
+            }
+
+            c0.isConnection = true;
+            c1.isConnectino = true;
+
+            r0.closestCells.push({
+                c0: c0,
+                c1: c1
+            });
+        }
+    }
+};
+CaveMazeCarver.prototype.linkRegions = function() {
+    for (var r of this.regions) {
+        for (var cc of r.closestCells) {
+            if (this.hasClearPath(cc.c0, cc.c1)) {
+                this.carveTunnel(cc.c0, cc.c1);
+            }
+        }
+    }
+    // //  6,43
+    // // 43,19
+    // var c0 = this.maze.getCell(6, 43);
+    // var c1 = this.maze.getCell(43, 19);
+
+    // this.carveTunnel(c0, c1);
+};
+CaveMazeCarver.prototype.carveTunnel = function(c0, c1, action) {
+    var x0 = c0.x;
+    var y0 = c0.y;
+    var x1 = c1.x;
+    var y1 = c1.y;
+
+    var dx = Math.abs(x1 - x0);
+    var dy = Math.abs(y1 - y0);
+    var sx = (x0 < x1) ? 1 : -1;
+    var sy = (y0 < y1) ? 1 : -1;
+    var err = dx - dy;
+
+    while (true) {
+        var cc = this.maze.getCell(x0, y0);
+        this.carve(cc);
+
+        if (x0 == x1 && y0 == y1) {
+            break;
+        }
+
+        var err2 = 2 * err;
+        if (err2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+        if (err2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+};
+CaveMazeCarver.prototype.hasClearPath = function(c0, c1) {
+    var x0 = c0.x;
+    var y0 = c0.y;
+    var x1 = c1.x;
+    var y1 = c1.y;
+
+    var dx = Math.abs(x1 - x0);
+    var dy = Math.abs(y1 - y0);
+    var sx = (x0 < x1) ? 1 : -1;
+    var sy = (y0 < y1) ? 1 : -1;
+    var err = dx - dy;
+
+    while (true) {
+        var cc = this.maze.getCell(x0, y0);
+        if ((cc !== c0 && cc != c1 && cc.visited) || (cc.region != -1 && cc.region != c0.region && cc.region != c1.region)) {
+            return false;
+        }
+
+        if (x0 == x1 && y0 == y1) {
+            break;
+        }
+
+        var err2 = 2 * err;
+        if (err2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+        if (err2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+
+    return true;
 };
 CaveMazeCarver.prototype.makeCaverns = function() {
     for (var g = 0; g < this.generations; g++) {
@@ -60,12 +174,30 @@ CaveMazeCarver.prototype.identifyRegions = function() {
 };
 CaveMazeCarver.prototype.floodFill = function(startCell) {
     var regionCells = [startCell];
+    var regionEdges = [];
+    var minX, maxX, minY, maxY, center;
     var ix = 0;
     while (ix < regionCells.length) {
         for (var nd of this.maze.directions) {
-            var n = this.maze.getCell(regionCells[ix].x + nd.x, regionCells[ix].y + nd.y);
+            var currentCell = regionCells[ix];
+            var n = this.maze.getCell(currentCell.x + nd.x, currentCell.y + nd.y);
+            if ((n === undefined || !n.visited) && regionEdges.indexOf(currentCell) === -1) {
+                regionEdges.push(currentCell);
+            }
             if (n !== undefined && n.visited && regionCells.indexOf(n) === -1) {
                 regionCells.push(n);
+                if (minX === undefined || minX.x > n.x) {
+                    minX = n;
+                }
+                if (maxX === undefined || maxX.x < n.x) {
+                    maxX = n;
+                }
+                if (minY === undefined || minY.y > n.y) {
+                    minY = n;
+                }
+                if (maxY === undefined || maxY.y < n.y) {
+                    maxY = n;
+                }
             }
         }
         ix += 1;
@@ -77,7 +209,23 @@ CaveMazeCarver.prototype.floodFill = function(startCell) {
             rc.visited = false;
         }
     } else {
-        this.regions[this.currentRegion] = regionCells;
+        // minX.isConnection = true;
+        // maxX.isConnection = true;
+        // minY.isConnection = true;
+        // maxY.isConnection = true;
+        center = this.maze.getCell(parseInt((minX.x + maxX.x) / 2), parseInt((minY.y + maxY.y) / 2));
+        // center.isConnection = true;
+        this.regions[this.currentRegion] = {
+            id: this.currentRegion + 1,
+            minX: minX,
+            maxX: maxX,
+            minY: minY,
+            maxY: maxY,
+            center: center,
+            regionCells: regionCells,
+            regionEdges: regionEdges,
+            closestCells: []
+        };
         this.startRegion();
         for (var rc of regionCells) {
             rc.region = this.currentRegion;
